d4d52a9256874a73b42d07b1878e43655b8f3935
Dale Baker-Allan <daleb1206@gmail.com>
2019-02-12 23:13:50 +0000

    Database connector script and login script created and tested.

---
+++ databaseConnector.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dalebaker-allan
+ * Date: 2019-02-12
+ * Time: 21:45
+ */
+
+// Database information
+$host = "mysql-server-1.macs.hw.ac.uk";
+$user = "db47";
+$pass = "7WwEnz9n99";
+$database = "db47";
+
+//create connection object
+$conn = new mysqli($host, $user, $pass, $database);
+
+//check for error
+if($conn->connect_error) {
+    die("Connection to the database failed." . $conn->connect_error);
+}
+
+/**
+ * As this is just the connector file for the database
+ * at the end of each query file, the connection to the database must be closed
+ *
+ * Usage: $conn->close()
+ * Where: $conn is the connection object
+ *
+ * If used at the end of this file, the connection will be closed
+ * before any query has been executed
+ */
---
+++ read/login.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dalebaker-allan
+ * Date: 2019-02-12
+ * Time: 21:44
+ */
+
+
+include('../databaseConnector.php');
+
+// Get data from the HTTP POST request
+// $_POST['...'], ... is defined in the sending of the post request
+//we need to sanitize the input to prevent SQL injection
+//mysqli_real_escape_string escapes special characters to prevent SQL injection
+//$conn refers to the connection object from databaseConnector.php
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+/**
+$username = $_POST['username'];
+$password = $_POST['password'];
+**/
+//create the query to execute
+$sql_query = "SELECT user.userID FROM user WHERE username = '$username' AND password = '$password'";
+
+//execute query and get results
+$result = $conn->query($sql_query);
+
+if($result->num_rows == 1) {
+    //we have a match, create an array to store all the rows
+    $rows = array();
+
+    //loop through all the rows in the result set
+    while($row = $result->fetch_assoc()) {
+        //add current row into array
+        $rows[] = $row;
+    }
+
+    //output as json encoded text
+    echo json_encode($rows);
+} else if($result->num_rows > 1) {
+    //we have duplicate values
+    //create an array with 'error' tag that maps to the error,
+    //this is so the json can encode as [{"error" : "..."}]
+    $arr = array('error' => 'There is more than one account! Contact the database administrator');
+    echo json_encode($arr);
+} else if($result->num_rows == 0) {
+    //no match
+    //create an array with 'error' tag that maps to the error,
+    //this is so the json can encode as [{"error" : "..."}]
+    $arr = array('error' => 'Login details are incorrect!');
+    echo json_encode($arr);
+}
+
+//ALWAYS CLOSE CONNECTION
+$conn->close();
+
+/**
+ * Example command to test post request from Linux/Mac terminal
+ * curl -v -H "Accept:application/json" -X POST -F "username=admin" -F "password=admin" http://www2.macs.hw.ac.uk/~db47/WheelyGoodBikes/DatabaseLayer/read/login.php
+ */
