a4859676585b70a82a4c63d4151b063fc75cc73f
Dale Baker-Allan <daleb1206@gmail.com>
2019-02-13 19:56:23 +0000

    Insert and Delete EmployeeAccount implemented

--- create/addEmployeeAccount.php
+++ create/addEmployeeAccount.php
@@ -8,6 +8,8 @@
 
 include("../databaseConnector.php");
 
+$conn = connect();
+
 //Get the data
 $username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
@@ -18,66 +20,13 @@ $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $phone = mysqli_real_escape_string($conn, $_POST['phone']);
 
+
 //first we create a query to add the user account and retrieve the user id
 //create the query to execute
-$sql_query = "INSERT INTO user VALUES (null, '$account_type', '$username', '$password');";
 //execute query and get results
+$sql_query = "INSERT INTO user VALUES (null, '$account_type', '$username', '$password');";
 
-/**
- * @TODO Fix an unknown error in inserting Employee Account
- * @BODY An unknown error is occuring (i.e. no error output using curl) when
- * attempting to insert into database. Data is inserted into user account but then fails.
- */
-if($conn->query($sql_query) === TRUE) {
-    //stored successfully, we can continue
-    //we need to get the user id of the inserted user
-    $sql_query = "SELECT user.userID FROM user WHERE username = '$username' AND password = '$password';";
-    $results = $conn->query($sql_query);
-    if($results->num_rows == 1) {
-        $user_id = $results['userID'];
-        echo $user_id . "\n";
-        //continue
-    } else {
-        echo "failed";
-        //failed, return error
-        $arr = array('error' => 'Could not get userID of new user!');
-        echo json_encode($arr);
-        die();
-    }
-
-    //insert into employee table
-    $sql_query = "INSERT INTO employees VALUES (null '$user_id', '$location_id');";
-    if($conn->query($sql_query) === TRUE) {
-        //success, continue
-
-        //we now need the employee ID
-        $sql_query = "SELECT employees.employeeID FROM employees WHERE employees.userID = '$user_id';";
-        $results = $conn->query($sql_query);
-        if($results->num_rows == 1) {
-            $employee_id = $results['employeeID'];
-            echo $employee_id . "\n";
-            //continue
-        } else {
-            //failed, return error
-            $arr = array('error' => 'Could not get employeeID of new user!');
-            echo json_encode($arr);
-            die();
-        }
-
-        //insert employee info
-        $sql_query = "INSERT INTO employee_info VALUES ('$employee_id', '$first_name', '$last_name', '$email', '$phone');";
-        if($conn->query($sql_query) === TRUE) {
-            echo "SUCCESS";
-            //then the user has been successfully added, finish up and return to user
-            $arr = array('success' => 'The user has been successfully added');
-            echo json_encode($arr);}
-    } else {
-        //error, terminate
-        $arr = array('error' => 'Could not insert employee into the database!');
-        echo json_encode($arr);
-        die();
-    }
-} else {
+if($conn->query($sql_query) !== TRUE) {
     //error, terminate
     //create an array with 'error' tag that maps to the error,
     //this is so the json can encode as [{"error" : "..."}]
@@ -85,6 +34,63 @@ if($conn->query($sql_query) === TRUE) {
     echo json_encode($arr);
     die();
 }
+
+//inserted, continue
+$sql_query = "SELECT userID FROM user WHERE username='$username' AND password='$password';";
+$results = $conn->query($sql_query);
+
+if($results->num_rows > 1 || $results->num_rows == 0) {
+    //failed
+    $arr = array('error' => 'Could not get new userID!', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+//get the first row
+$row = $results->fetch_assoc();
+//row['<name>'] are the same as the database columns
+$user_id = $row['userID'];
+
+$sql_query = "INSERT INTO employees VALUES (null, '$user_id', '$location_id');";
+
+if($conn->query($sql_query) !== TRUE) {
+    //error, terminate
+    //create an array with 'error' tag that maps to the error,
+    //this is so the json can encode as [{"error" : "..."}]
+    $arr = array('error' => 'Could not insert employee into the database!', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+//success, continue
+
+$sql_query = "SELECT employeeID FROM employees WHERE userID='$user_id';";
+$results = $conn->query($sql_query);
+
+if($results->num_rows > 1 || $results->num_rows == 0) {
+    $arr = array('error' => 'Could not get new employeeID!', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+//get the first row
+$row = $results->fetch_assoc();
+//row['<name>'] are the same as the database columns
+$employee_id = $row['employeeID'];
+
+//success, continue
+$sql_query = "INSERT INTO employee_info VALUES ('$employee_id', '$first_name', '$last_name', '$email', '$phone');";
+
+if($conn->query($sql_query) !== TRUE) {
+    //error, terminate
+    $arr = array('error' => 'Could not insert employee info into the database!', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+//we have succeeded
+$arr = array('success' => 'User insertion completed');
+echo json_encode($arr);
 
 //ALWAYS CLOSE CONNECTION
 $conn->close();
--- delete/deleteEmployeeAccount.php
+++ delete/deleteEmployeeAccount.php
@@ -8,4 +8,40 @@
 
 include("../databaseConnector.php");
 
+//get connection object
+$conn = connect();
+
+//data fields
 $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+$employee_id = mysqli_real_escape_string($conn, $_POST['employee_id']);
+
+//sql queries
+$sql_query = "DELETE FROM employee_info WHERE employeeID = '$employee_id'";
+if($conn->query($sql_query) !== TRUE) {
+    //error, terminate
+    $arr = array('error' => 'Could not delete Employee information.', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+$sql_query = "DELETE FROM employees WHERE employeeID = '$employee_id' AND userID = '$user_id'"; // .= operator concatenates to the string
+if($conn->query($sql_query) !== TRUE) {
+    //error, terminate
+    $arr = array('error' => 'Could not delete Employee.', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+$sql_query = "DELETE FROM user WHERE userID = '$user_id'";
+if($conn->query($sql_query) !== TRUE) {
+    //error, terminate
+    $arr = array('error' => 'Could not delete user account.', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+$arr = array('success' => 'User deleted');
+echo json_encode($arr);
+
+$conn->close();
+
