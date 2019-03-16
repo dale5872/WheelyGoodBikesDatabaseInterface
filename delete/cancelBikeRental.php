e0d2202c272a560fa0a546fca1b0ecb8d511cb9e
inkarbegim <48454291+inkarbegim@users.noreply.github.com>
2019-03-16 23:45:18 +0000

    Add files via upload

---
+++ delete/cancelBikeRental.php
@@ -0,0 +1,23 @@
+<?php
+
+include("databaseConnector.php");
+//connection object
+$conn = connect();
+//data
+$bike_rental_id = mysqli_real_escape_string($conn, $_POST['bike_rental_id']);
+$user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+
+$sql_query = "DELETE FROM bike_rentals WHERE userID = '$user_id' AND bikeRentalId='$bike_rental_id'";
+
+if($conn->query($sql_query) !== TRUE) {
+    //error, terminate
+    $arr = array('status' => 'error', 'message' => 'Could not delete info from the database!', 'stackTrace' => $conn->error);
+    echo json_encode($arr);
+    die();
+}
+
+//we have succeeded
+$arr = array('status' => 'success');
+echo json_encode($arr);
+//ALWAYS CLOSE CONNECTION
+$conn->close();
