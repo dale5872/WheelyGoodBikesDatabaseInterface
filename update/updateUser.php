c4ebb9583b4c16c94931204fbe809e62d81e0400
inkarbegim <48454291+inkarbegim@users.noreply.github.com>
2019-03-17 14:28:43 +0000

    Add files via upload

--- update/updateUser.php
+++ update/updateUser.php
@@ -4,8 +4,7 @@ include("databaseConnector.php");
 //connection object
 $conn = connect();
 //data
-$username = mysqli_real_escape_string($conn, $_POST['username']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
+$user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
 
 //takes input regarding what table to edit, what column to edit, what information to change
 $update_table = mysqli_real_escape_string($conn, $_POST['update_table']);
@@ -16,19 +15,6 @@ $update_info = mysqli_real_escape_string($conn, $_POST['update_info']);
 
 
 
-$sql_query = "SELECT userID FROM user WHERE username='$username' AND password='$password';";
-
-$results = $conn->query($sql_query);
-if($results->num_rows > 1 || $results->num_rows == 0) {
-    //failed
-    $arr = array('status' => 'error', 'message' => 'Could not get new userID!', 'stackTrace' => $conn->error);
-    echo json_encode($arr);
-    die();
-}
-//get the first row
-$row = $results->fetch_assoc();
-//row['<name>'] are the same as the database columns
-$user_id = $row['userID'];
 
 
 $sql_query = "UPDATE $update_table SET $update_col = '$update_info' WHERE userID = '$user_id' ";
