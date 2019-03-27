6ab2e0e848d4f10479f4f1d1531543d3df440e43
inkarbegim <ik15@hw.ac.uk>
2019-03-27 22:33:55 +0000

    Update addBikeRentalApp.php

--- create/addBikeRentalApp.php
+++ create/addBikeRentalApp.php
@@ -4,24 +4,26 @@ include('../databaseConnector.php');
 //get connection object
 $conn = connect();
 
-$bike_id = mysqli_real_escape_string($conn, $_POST['bike_id']);
+$return_time = mysqli_real_escape_string($conn, $_POST['return_time']);
+$start_time = mysqli_real_escape_string($conn, $_POST['start_time']);
 $location = mysqli_real_escape_string($conn, $_POST['location']);
 $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-$price = mysqli_real_escape_string($conn, $_POST['price']);
+$bike_id = mysqli_real_escape_string($conn, $_POST['bike_id']);
 $status = mysqli_real_escape_string($conn, $_POST['status']);
-$start_time = mysqli_real_escape_string($conn, $_POST['start_time']);
-$return_time = mysqli_real_escape_string($conn, $_POST['return_time']);
+$price = mysqli_real_escape_string($conn, $_POST['price']);
 
 $sql_query = "INSERT INTO bike_rentals VALUES (null, '$user_id', '$bike_id', '$location', '$price', '$start_time', '$return_time', '$status');";
 
 if($conn->query($sql_query) !== TRUE) {
-    //error, terminate
-    $arr = array('status' => 'error', 'message' => 'Could not create new bike type', 'stackTrace' => $conn->error);
-    echo json_encode($arr);
-    die();
+	//error, terminate
+	$arr = array('status' => 'error',
+	'message' => 'Could not create new bike type',
+	'stackTrace' => $conn->error);
+	echo json_encode($arr);
+	die();
 }
 
-$arr = array('status' => 'success', 'id' => mysql_insert_id($conn));
+$arr = array('status' => 'success');
 echo json_encode($arr);
 
 $conn->close();
